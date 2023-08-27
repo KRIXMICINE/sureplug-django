15c9377aa2464937525485373d4f862eba0b15c1
Ifediorah Christopher <michaelfeelings@gmail.com>
2023-08-27 03:48:30 +0100

    This is the first commit.

---
+++ sureplug/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from store.models import product
+from category.models import category
+
+
+
+def home(request):    
+    products = product.objects.all().filter(is_available= True)    
+    context = {'products': products}
+    return render(request, 'home.html', context)
